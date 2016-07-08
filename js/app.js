13618dc6500f102a2fa51c7bdb3273dc93ffd5bc
Keith Underwood <kuwood@users.noreply.github.com>
2016-07-07 20:08:26 -0600

    Make code review edits

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
-function Questions(strong, salty, bitter, sweet, fruity) {
-  this.strong = strong;
-  this.salty = salty;
-  this.bitter = bitter;
-  this.sweet = sweet;
-  this.fruity = fruity;
-}
-
-function Ingredients(strong, salty, bitter, sweet, fruity) {
+function Categories(strong, salty, bitter, sweet, fruity) {
   this.strong = strong;
   this.salty = salty;
   this.bitter = bitter;
@@ -24,7 +16,7 @@ function getAllKeyValues(object) {
   for (var property in object) {
     object[property].forEach(function(v) {
       values.push(v);
-    })
+    });
   }
   return values;
 }
@@ -37,28 +29,29 @@ function generateQuestions() {
   $("#fruity-question").text(bartenderQuestions.fruity);
 }
 
-var bartenderQuestions = new Questions("Do ye like yer drinks strong?",
+var bartenderQuestions = new Categories("Do ye like yer drinks strong?",
   "Do ye like it with a salty tang?",
   "Are ye a lubber who likes it bitter?",
   "Would ye like a bit of sweetness with yer poison?",
   "Are ye one for a fruity finish?"
 );
 
-var ingredients = new Ingredients(["Glug of rum", "slug of whisky", "splash of gin"], ["Olive on a stick", "salt-dusted rim", "rasher of bacon"], ["Shake of bitters", "splash of tonic", "twist of lemon peel"], ["Sugar cube", "spoonful of honey", "splash of cola"], ["Slice of orange", "dash of cassis", "cherry on top"]);
+var ingredients = new Categories(["Glug of rum", "slug of whisky", "splash of gin"], ["Olive on a stick", "salt-dusted rim", "rasher of bacon"], ["Shake of bitters", "splash of tonic", "twist of lemon peel"], ["Sugar cube", "spoonful of honey", "splash of cola"], ["Slice of orange", "dash of cassis", "cherry on top"]);
 
-function prefsCheck(object) {
-  a = Object.keys(object);
+function prefsCheck(preferences) {
+  a = Object.keys(preferences);
   for (var taste in a) {
     var keyName = a[taste];
     if ($("#" + keyName + "-yes").is(":checked")) {
       userPrefs[keyName] = true;
     } else {
-      userPrefs[keyname] = false;
+      userPrefs[keyName] = false;
     }
   }
 }
 
 function getRandomInt() {
+  //returns a random number up to 3(number of indexes in the array)
   return Math.floor(Math.random() * (3 - 0)) + 0;
 }
 
@@ -75,12 +68,12 @@ function Bartender() {
       }
     }
     return spicyMeatball;
-  }
+  };
 }
 
 var myPantry = new Pantry(getAllKeyValues(ingredients));
 
-var userPrefs = new Questions();
+var userPrefs = new Categories();
 
 var theBartender = new Bartender();
 
@@ -90,5 +83,5 @@ $(function() {
     e.preventDefault();
     prefsCheck(userPrefs);
     $(".your-drink").text(theBartender.getDrink(userPrefs));
-  })
-})
+  });
+});
